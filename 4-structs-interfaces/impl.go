9395bb3e81e17ef3e3e6a50f80c5fa302b005041
Дмитрий Ивахненко <ivahnencko01@gmail.com>
2025-01-16 09:55:11 +0300

    WIP

---
+++ 4-structs-interfaces/impl.go
@@ -0,0 +1,33 @@
+package arrays_and_slices
+
+import "math"
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2.0 * (r.Width + r.Height)
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
